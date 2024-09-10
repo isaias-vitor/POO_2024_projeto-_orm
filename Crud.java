c53ac930ec703da0ae6046a6b3411029cba71bb7
Isaias Vitor Silva Oliveira <93414564+isaias-vitor@users.noreply.github.com>
2024-09-09 23:51:54 -0300

    Update Crud.java

--- Crud.java
+++ Crud.java
@@ -13,11 +13,7 @@ import java.time.LocalDate;
 public class Crud {
 
     public static void main(String[] args) {
-        
-        UserDao userDao = new UserDao();
 
-        userDao.saveOrUpdate(new User(1L, "Ana Zaira", "a.zaira@mail.com", "123", LocalDate.now(), true));
-        userDao.saveOrUpdate(new User(2L, "Beatriz Yana", "b.yana@mail.com", "456", LocalDate.now(), true));
         
     }
 }
